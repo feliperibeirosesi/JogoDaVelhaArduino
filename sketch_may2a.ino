3740caf9de1f65ada4fae9d5559fdff47af3cb40
feliperibeirosesi <163848058+feliperibeirosesi@users.noreply.github.com>
2024-05-02 15:42:19 -0300

    Arduino

---
+++ sketch_may2a.ino
@@ -0,0 +1,52 @@
+bool validaPosicao(string entrada);
+
+int linha; //Classifiquei linha como inteiro
+
+int coluna; //Classifiquei coluna como inteiro
+
+int velha;
+velha = 1;
+
+string Jogada;
+
+bool haVencedor; //Classifiquei haVencedor como Booleano
+haVencedor = false; //e o atribui como falso
+
+int index 
+
+int tabuleiro(9); //primeiro passo para criar uma matriz
+
+for (index = 0; index <= 8; index++){
+  tabuleiro[index] = 0;
+
+}
+int JogadordaVez;
+JogadordaVez = 1;
+
+
+void setup() {
+Serial.begin (9600)
+do{
+  Serial.println(tabuleiro[0] tabuleiro[1] tabuleiro[2]);
+  Serial.println(tabuleiro[3] tabuleiro[4] tabuleiro[5]);
+  Serial.println(tabuleiro[6] tabuleiro[7] tabuleiro[8]);
+ 
+ Jogada = "";
+
+Serial.println("Digite a sua casa Jogador " & JogadordaVez );
+String ler = Serial.readString() 
+
+if(validaPosicao(jogada)){
+
+ linha = ler[0].parseInt(); 
+
+}
+
+}
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
